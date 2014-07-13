01a10e23f197bbf6a826581f9daa31be3ea074c3
Abhishek N Jain <abhisheknjain30@gmail.com>
2014-07-13 23:59:41 +0530

    first commit

---
+++ smartpie profile/smartpie profile/js/for-profile.js
@@ -0,0 +1,9 @@
+function statusedit(){
+	 	document.getElementById("status-viewonly").style.display="none";
+	 	document.getElementById("status-edit").style.display="inline-block";
+}
+
+function statusupdate(){
+	 	document.getElementById("status-edit").style.display="none";
+	 	document.getElementById("status-viewonly").style.display="inline-block";
+}
